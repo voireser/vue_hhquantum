d7498f2a6c5a877508bfec2a8f11997803106380
Voireser <voir@VoirdeMacBook-Pro.local>
2018-03-20 20:07:37 +0800

    fix wish.vue

--- src/components/showApp/wish.vue
+++ src/components/showApp/wish.vue
@@ -3,7 +3,7 @@
         <p><i class="iconfont">&#xe640;</i></p>
         <p>
             <span>{{wish.word_1}}</span>
-            <span>{{wish.word_2}}</span>
+            <span v-html="wish.word_2"></span>
         </p>
     </div>
 </template>
@@ -14,7 +14,7 @@ export default {
         return {
             wish: {
                 word_1: '我们的愿景',
-                word_2: '推动生活环境的可持续发展，创造更健康的人居环境打造您私属的环境管家'
+                word_2: '推动生活环境的可持续发展，创造更健康的人居环境<br>打造您私属的环境管家'
             }
         }
     }
